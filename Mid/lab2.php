f98d84a77f6f797da3a966b5b792b1f2b35943c2
Sajin Ahamed <sajinahamed1867@gmail.com>
2025-11-17 12:07:06 +0600

    modied lab3.php

--- Mid/lab2.php
+++ Mid/lab2.php
@@ -1,11 +1,11 @@
 <DOCTYPE html>
 <html lang ="en">
 
-    <head>
-        <meta charset = "UTF-8">
-        <title>participant registration and activity selection</title>
-            <style>
-                body {
+ <head>
+<meta charset = "UTF-8">
+<title>participant registration and activity selection</title>
+<style>
+          body {
             font-family: Arial, sans-serif;
                 background :  #e8f0ff ;
                 padding: 20 px;
@@ -18,8 +18,44 @@
             padding: 20px;
             border-radius: 8px;
             box-shadow: 0 0 10px rgba (0,0,0.1)
-            margin-buttom: 35px;
+            margin - buttom : 35px;
            }  
+            h2{
+                text-align: center;
+                margin - bottom: 20px;
+
+
+            }
+            input{
+
+                width:100%;
+                padding: 8px;
+                margin: 8px 0;
+                border : 1px solid #ccccccff;
+                border -radius : 5px;
+            }
+            bottom {
+
+                padding: 8px 15px;
+                background: rgba(75, 0, 119, 0.73);
+                color:white;
+                border :none;
+                border radius: 5px;
+                cursor: pointer;
+                margin-top :10px;
+            }
+             button:hover {
+                background: rgba(0, 20, 85, 1);
+             }
+             .success-box {
+                background :#d6f5d6;
+                padding :15 px;
+                border-radius:5px;
+                margin-top:15px;
+                border:1px solid green;
+
+             }
+             
            
            
 
