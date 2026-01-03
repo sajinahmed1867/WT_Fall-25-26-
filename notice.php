c562dfbbdc41492aa8ed45d62a09b8ffef331343
Sajin Ahamed <sajinahamed1867@gmail.com>
2026-01-03 19:15:20 +0600

    add notice.php

---
+++ notice.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+<title>Notice</title>
+</head>
+<body>
+
+<div class="box">
+<h2>Notices</h2>
+
+<ul>
+<li>📢 Class test on Sunday</li>
+<li>📢 Assignment submission next week</li>
+<li>📢 Lab class postponed</li>
+<li>📢 today class cancle</li>
+<li>📢 tomorrow lab exam webtech</li>
+<li>📢 final ptoject defence 20/01/2026</li>
+</ul>
+
+<a href="dashboard.php">Back</a>
+</div>
+
+</body>
+</html>
