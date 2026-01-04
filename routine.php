b7b12d7bb3493e3029318fd8401249754809052d
Sajin Ahamed <sajinahamed1867@gmail.com>
2026-01-04 23:05:19 +0600

    add routine.php

---
+++ routine.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+<title>Class Routine</title>
+</head>
+<body>
+
+<div class="box">
+<h2>Class Routine</h2>
+
+<table border="1" width="100%">
+<tr><th>Day</th><th>Course</th></tr>
+<tr><td>Sunday</td><td>Web Technology</td></tr>
+<tr><td>Tuesday</td><td>Database Systems</td></tr>
+<tr><td>Thursday</td><td>AI</td></tr>
+</table>
+
+<a href="dashboard.php">Back</a>
+</div>
+
+</body>
+</html>
