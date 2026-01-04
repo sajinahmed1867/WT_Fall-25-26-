50d190696ee9cee70d37449c8a0e5f91ccd7c80d
Sajin Ahamed <sajinahamed1867@gmail.com>
2026-01-04 23:04:10 +0600

    password.php

---
+++ password.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+<title>Change Password</title>
+</head>
+<body>
+
+<div class="box">
+<h2>Change Password</h2>
+
+<input type="password" placeholder="Old Password">
+<input type="password" placeholder="New Password">
+<input type="password" placeholder="Confirm Password">
+
+<button onclick="alert('Password Changed (Demo)')">Change</button>
+<a href="dashboard.php">Back</a>
+</div>
+
+</body>
+</html>
